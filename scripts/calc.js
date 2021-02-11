0d0b77551e2496ced39b03ec528dd40e887559bc
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-02-11 19:22:47 +0000

    Added in subtraction method with tests
    For testing out using Jasmine without watching a video for memory I created a subtraction function which takes 2 parameters n1 number to subtract from n2

--- scripts/calc.js
+++ scripts/calc.js
@@ -9,3 +9,11 @@ Calculator.prototype.add = function(number){
     alert("Error!");
   }
 }
+
+Calculator.prototype.subtract = function(n1, n2){
+  if(typeof(n1) == "number" && typeof(n2) == "number"){
+    this.value = n1 - n2;
+  } else {
+    alert("Error!");
+  }
+}
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,15 +1,37 @@
 describe("Calculator", function() {
+  beforeEach(function(){
+     calc = new Calculator;
+  });
   describe("Addition tests", function(){
     it("should return 42", function(){
-      expect(addition(20,22)).toBe(42)
+      calc.add(22);
+      calc.add(20);
+      expect(calc.value).toBe(42);
     });
     it("should return 26", function(){
-      expect(addition(7,19)).toBe(26);
+      calc.add(7);
+      calc.add(19);
+      expect(calc.value).toBe(26);
     });
     it("should return an error if we don't supply two numbers", function(){
        spyOn(window, "alert");
-       addition("Hitchhikers", "Guide");
+       calc.add("Hitchhikers");
        expect(window.alert).toHaveBeenCalledWith("Error!");
+    });
+  });
+  describe("Subtraction tests", function(){
+    it("Should return 5", function(){
+      calc.subtract(10, 5);
+      expect(calc.value).toBe(5);
+    });
+    it("Should return 15", function(){
+      calc.subtract(20, 5);
+      expect(calc.value).toBe(15);
+    });
+    it("Should return an error if we don't supply two numbers", function(){
+      spyOn(window, "alert");
+      calc.subtract(1);
+      expect(window.alert).toHaveBeenCalledWith("Error!");
     });
   });
 });
